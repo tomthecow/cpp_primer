7c7d66fe13ddd1fd68a2770aeaaf5166f6a2de3e
tomthecow <tom.michael.daniels@gmail.com>
2018-02-05 19:24:46 -0500

    chapter3/

--- chapter3/14.cc
+++ chapter3/14.cc
@@ -1,3 +1,9 @@
+//read some text into a vector, storing each word in the input as
+//an element in the vector. Transform each word into uppercase 
+//letters. Print the transformed elements from the vector, 
+//printing eight words to a line. 
+
+
 #include<iostream>
 #include<string>
 #include<vector>
@@ -11,13 +17,47 @@ using std::vector;
 int main()
 {
     vector<string> input;
+    string n;
+    bool output_flag = 0, input_flag = 0;
 
-    cout << "Please enter some text.\n";
-    cin >> input.push_back();
+    cout << "Please enter some text.\n"
+        << "Hit ctrl-d to end input\n";
+    while(cin >> n){
+        input.push_back(n);
+    }
 
-    for (int i = 0; i != input.size(); i++){
-        cout << input[i] << endl;
+    if (input.empty() == 0) {
+        for (int i = 0; i != input.size(); i++){
+            if(input_flag == 0){
+                cout << "\nYour input was: \n";
+                input_flag = 1;
+            }
+            cout << input[i] << " " ;
+        }
+        
+        cout << "\n\nContents in uppercase: \n";
+
+        for (int i = 0; i != input.size(); i++){
+            if (output_flag == 0){
+                output_flag = 1;
+                cout << "Output : ";
+            }
+        
+            int j = 0;
+
+            while(input[i][j]){
+                char a = toupper(input[i][j]);
+                cout << a;
+                j++;
+            }
+            cout << " ";
+        }
+
+        cout << endl;
+
     }
 
     return 0;
+    
+    
 }
